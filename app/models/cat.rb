ad1d21a3e241657dcdc3f8999a7f161789df4b89
mezzolite <mez.charney@gmail.com>
2019-12-02 16:43:52 -0700

    adds solutions in cat model

--- app/models/cat.rb
+++ app/models/cat.rb
@@ -1,5 +1,32 @@
 class Cat < ApplicationRecord
   belongs_to :owner
 
+  def self.names
+    distinct.pluck(:name)
+  end
+
+  def self.fluffies
+    where(fluffy: true)
+  end
   
+  def self.olds
+    where('age > ?', 8)
+  end
+
+  def self.middle_aged
+    where(age: (1..8))
+  end
+
+  def self.youngest_cats(num)
+    order(age: :asc).limit(num)
+  end
+
+  def self.oldest_cats(num)
+    order(age: :desc).limit(num)
+  end
+
+  def self.age_breakdown
+    order(age: :asc).group(:age).count
+  end
+
 end
--- app/models/event.rb
+++ app/models/event.rb
@@ -1,5 +1,6 @@
 class Event < ApplicationRecord
   belongs_to :owner
+  has_many :cats, through :owner
 
  
 end
